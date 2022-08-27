b4eeb48d6909985f22be93dee614474b35da88df
lukorito <jonesdeelder@gmail.com>
2022-08-27 17:51:33 +0300

    Finished set-up

--- src/server.ts
+++ src/server.ts
@@ -31,6 +31,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   //! END @TODO1
   
+  
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
