fc2b1519cb7a3ea1339576393214c8404e00b917
lukorito <jonesdeelder@gmail.com>
2022-08-28 13:23:21 +0300

    Edited parts of Js to Ts

--- src/server.ts
+++ src/server.ts
@@ -34,11 +34,11 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //! END @TODO1
 
  app.get( "/filteredimage", async ( req: Request, res:Response ) => {
-    const  image_url  = req.query.image_url.toString();
+    const  image_url: string  = req.query.image_url.toString();
     if ( !image_url ) {
       return res.status(400).send(`Image url is required`);
     }
-    const filtered_image = await filterImageFromURL(image_url);
+    const filtered_image: string = await filterImageFromURL(image_url);
 
     res.status(200).sendFile(filtered_image, () => {
       deleteLocalFiles([filtered_image]);
